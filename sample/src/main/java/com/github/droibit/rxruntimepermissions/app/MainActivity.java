896299ea60d026645bb75761dcaf748933580879
Shinya Kumagai <roomful.rooms@gmail.com>
2016-09-11 13:54:41 +0900

    Implement sample app

--- sample/src/main/java/com/github/droibit/rxruntimepermissions/app/MainActivity.java
+++ sample/src/main/java/com/github/droibit/rxruntimepermissions/app/MainActivity.java
@@ -1,6 +1,11 @@
 package com.github.droibit.rxruntimepermissions.app;
 
+import com.github.droibit.rxruntimepermissions.RxRuntimePermissions;
+import com.jakewharton.rxbinding.view.RxView;
+
+import android.Manifest;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,45 +13,43 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import rx.Observable;
+import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CAMERA = 1;
+
+    private final RxRuntimePermissions rxRuntimePermissions = new RxRuntimePermissions();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final Observable<Void> trigger = RxView.clicks(fab);
+        rxRuntimePermissions.from(this)
+                .on(trigger)
+                .requestPermissions(REQUEST_CAMERA, Manifest.permission.CAMERA)
+                .subscribe(new Action1<Boolean>() {
+                    @Override
+                    public void call(Boolean granted) {
+                        final String msg = granted ? "Granted" : "Denied";
+                        Toast.makeText(MainActivity.this, "Camera Permission: " + msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        rxRuntimePermissions.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
